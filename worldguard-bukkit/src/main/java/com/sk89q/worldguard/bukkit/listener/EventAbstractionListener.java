282cc172992aea39cfe36620cf53a01d90879ebd
daniel-skopek <daniel.skopek1@gmail.com>
2026-01-10 14:53:01 +0100

    Removed debug message

--- worldguard-bukkit/src/main/java/com/sk89q/worldguard/bukkit/listener/EventAbstractionListener.java
+++ worldguard-bukkit/src/main/java/com/sk89q/worldguard/bukkit/listener/EventAbstractionListener.java
@@ -952,7 +952,6 @@ public class EventAbstractionListener extends AbstractListener {
             ItemStack item = event.getItemDrop().getItemStack();
 
             if (player.getInventory().firstEmpty() != -1) {
-                player.sendMessage("1");
                 player.updateInventory();
                 return;
             }
